410c498531236c071965ac00bf70c1c830f76154
kulotskiI <ishielmishma24@gmail.com>
2024-11-04 18:49:57 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let name = "Ishiel Mishma";
+let age = 20;
+let favnum = 24;
+let favcolor = "black";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favorate Number : " + favnum);
+console.log("Favorate Color : " + favcolor);
